33717c668c53f2fb657429ebf64c0d954c24d209
Twamev <87744709+Twamev@users.noreply.github.com>
2021-07-27 10:24:34 +0530

    Add files via upload
    code to know grade according to marks

---
+++ result.py
@@ -0,0 +1,14 @@
+total = int(input('Enter your total marks : '))
+if total > 90:
+	print('Grade A\nexcellent')
+elif total > 80:
+	print('Grade B\nvery good')
+elif total > 70:
+	print('Grade C\ngood')
+elif total > 60:
+	print('Grade D\nappreciable')
+else:
+	print('Grade F\nneed hard work')	
+
+
+print ('\nThank you.\nHave a nice day')			
