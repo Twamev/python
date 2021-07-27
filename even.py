b5a9b5c14ee98a945eb222ce24d56e60ef6be38f
Twamev <87744709+Twamev@users.noreply.github.com>
2021-07-27 09:49:42 +0530

    Update even.py

--- even.py
+++ even.py
@@ -1,7 +1,7 @@
 NUM = int(input('Entre Your Number'))
 if NUM%2:
-	print(f'{NUM} is a odd number.')
-else:
 	print(f'{NUM} is a even number.')
+else:
+	print(f'{NUM} is a odd number.')
 
 print('\nrest of program!!')
